3872d6de23603cc320038eb5b0a8624fdadae200
Branko Juric <branko.juric@gmail.com>
2015-02-09 11:10:42 +1100

    add more env repl options
    env -v|-visible to display visible scopes
    env -f|-feature to display feature scope
    env -a|-all to display all scopes

--- src/main/scala/gwen/eval/GwenREPL.scala
+++ src/main/scala/gwen/eval/GwenREPL.scala
@@ -65,9 +65,11 @@ class GwenREPL[T <: EnvContext](val interpreter: GwenInterpreter[T], val env: T)
    */
   private def eval(input: String): Option[String] = input.trim match {
     case "" => Some("[noop]")
-    case "env" | "env -visible" => 
+    case "env" | "env -v" | "env -visible" => 
       Some(Json.prettyPrint(env.visibleJson))
-    case "env -all" => 
+    case "env -f" | "env -feature" => 
+      Some(Json.prettyPrint(env.featureScope.json))
+    case "env -a" | "env -all" => 
       Some(Json.prettyPrint(env.json))
     case "exit" | "bye" | "quit" => 
       reader.getHistory().asInstanceOf[FileHistory].flush()
