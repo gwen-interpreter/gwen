78bb2527a36e3c45fbd5a027930737743ce009d5
Branko Juric <branko.juric@gmail.com>
2016-09-12 22:19:34 +1000

    add engine tests

--- src/test/scala/gwen/eval/EvalEngineTest.scala
+++ src/test/scala/gwen/eval/EvalEngineTest.scala
@@ -21,21 +21,64 @@ import org.scalatest.Matchers
 import gwen.dsl.Step
 import gwen.dsl.StepKeyword
 import gwen.errors.UndefinedStepException
+import gwen.eval.support.DefaultEngineSupport
+import gwen.dsl.StatusKeyword
+import gwen.dsl.Scenario
+import gwen.dsl.Tag
 
 class TestEnvContext(val options: GwenOptions, val scopes: ScopedDataStack) extends EnvContext(options, scopes)
-class TestEvalEngine extends EvalEngine[TestEnvContext] {
+class TestEvalEngine extends EvalEngine[TestEnvContext] with DefaultEngineSupport[TestEnvContext] {
   override def init(options: GwenOptions, scopes: ScopedDataStack): TestEnvContext = new TestEnvContext(options, scopes)
+  override def evaluate(step: Step, env: TestEnvContext) {
+    super.evaluate(step, env)
+  }
 }
   
 class EvalEngineTest extends FlatSpec with Matchers {
 
   val engine = new TestEvalEngine
-  val env = engine.init(new GwenOptions(), new ScopedDataStack())
   
   "Unsupported step" should "fail with UnsupportedStepException" in {
+    val env = engine.init(new GwenOptions(), new ScopedDataStack())
     intercept[UndefinedStepException] {
       engine.evaluate(Step(StepKeyword.Given, " I am unsupported"), env)
     }
+  }
+  
+  "Step that fails interpolation" should "not be evaluated" in {
+    val env = engine.init(new GwenOptions(), new ScopedDataStack())
+    var step = Step(StepKeyword.Given, """x is "${y}"""")
+    step = engine.evaluateStep(step, env)
+    step.evalStatus.status should be (StatusKeyword.Failed)
+    env.scopes.getOpt("x") should be (None)
+    step.stepDef should be (None)
+  }
+  
+  "Step that passes interpolation" should "should be evaluated" in {
+    val env = engine.init(new GwenOptions(), new ScopedDataStack())
+    env.scopes.set("y", "1")
+    var step = Step(StepKeyword.Given, """x is "${y}"""")
+    step = engine.evaluateStep(step, env)
+    step.evalStatus.status should be (StatusKeyword.Passed)
+    env.scopes.get("x") should be ("1")
+    step.stepDef should be (None)
+  }
+  
+  "Step that is a stepdef" should "should be evaluated" in {
+    val env = engine.init(new GwenOptions(), new ScopedDataStack())
+    val stepDef = Scenario(List[Tag]("@StepDef"), "I assign x, y, and z", Nil, None, List(
+        Step(StepKeyword.Given, """x is "1""""),
+        Step(StepKeyword.When, """y is "2""""),
+        Step(StepKeyword.Then, """z is "3"""")))
+    env.addStepDef(stepDef)
+    env.scopes.set("y", "1")
+    var step = Step(StepKeyword.Given, "I assign x, y, and z")
+    step = engine.evaluateStep(step, env)
+    step.evalStatus.status should be (StatusKeyword.Passed)
+    env.scopes.get("x") should be ("1")
+    env.scopes.get("y") should be ("2")
+    env.scopes.get("z") should be ("3")
+    step.stepDef should not be (None)
   }
   
 }
