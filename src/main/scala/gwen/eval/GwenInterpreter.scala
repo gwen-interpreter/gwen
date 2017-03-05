d436cd2391903a9a5618616d390774a3444f4bea
Branko Juric <branko.juric@gmail.com>
2017-03-05 23:16:04 +1100

    remove printStackTrace line

--- src/main/scala/gwen/eval/GwenInterpreter.scala
+++ src/main/scala/gwen/eval/GwenInterpreter.scala
@@ -129,7 +129,6 @@ class GwenInterpreter[T <: EnvContext] extends GwenInfo with GherkinParser with 
             }
           }
         case Failure(e) =>
-          e.printStackTrace()
           parsingError(e.toString)
       }
     }).getOrElse(None tap { _ => logger.warn(s"Skipped missing feature file: ${unit.featureFile.getPath}") })
