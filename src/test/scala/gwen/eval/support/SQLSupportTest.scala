dbf5bec86a07bb9f8e374b09eb030de1c650ffd8
Alexandru Cuciureanu <alexandru.cuciureanu@gmail.com>
2018-03-24 10:55:56 +0200

    updated Copyright

--- src/test/scala/gwen/eval/support/SQLSupportTest.scala
+++ src/test/scala/gwen/eval/support/SQLSupportTest.scala
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015 Branko Juric, Brady Wood
+ * Copyright 2018 Alexandru Cuciureanu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
