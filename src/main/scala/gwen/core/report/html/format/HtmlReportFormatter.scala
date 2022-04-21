dd0354b632c0332784a0cc365b62ddfb93ca4c9b
Branko Juric <branko.juric@gmail.com>
2022-04-21 18:00:22 +1000

    fix attachments dropdown bg

--- src/main/scala/gwen/core/report/html/format/HtmlReportFormatter.scala
+++ src/main/scala/gwen/core/report/html/format/HtmlReportFormatter.scala
@@ -322,7 +322,7 @@ object HtmlReportFormatter {
   }
 
   private [format] def formatAttachmentsDropdown(name: String, baseDir: Option[String], attachments: List[(String, File)], status: StatusKeyword, hrefFormatter: File => String): TypedTag[String] = { 
-    div(`class` := s"dropdown bg-${bgStatus(status)}",
+    div(`class` := s"dropdown",
       button(`class` := s"btn btn-${cssStatus(status)} bg-${bgStatus(status)} dropdown-toggle", attr("type") := "button", attr("data-toggle") := "dropdown", style := "position: relative; top: -0.5px;",
         strong(
           name
