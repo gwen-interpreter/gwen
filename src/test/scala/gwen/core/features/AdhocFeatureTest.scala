59c685b3555f3f85c1659bdf7a7a09a450419685
Branko Juric <branko.juric@gmail.com>
2022-10-17 10:09:17 +1100

    adjust logo in html report

--- src/test/scala/gwen/core/features/AdhocFeatureTest.scala
+++ src/test/scala/gwen/core/features/AdhocFeatureTest.scala
@@ -26,7 +26,7 @@ import java.io.File
 
 class AdhocFeatureTest extends BaseTest {
 
-  val feature = "src/test/features/parsing/ColonInStepDefName.feature"
+  val feature = "src/test/features/chars/ColonInStepDefName.feature"
 
   val interpreter = GwenInterpreter()
 
